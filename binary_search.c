d6cd149b6fa928ce4a11b383a4ad583f3d8a8111
innocent <innoxuganda@gmail.com>
2025-02-28 12:53:37 +0300

    binary search

---
+++ binary_search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target){
+    int left = 0;
+    int right = n-1;
+    while(left<=right){
+        int mid = (left + right) / 2;
+        printf("left - right: %d - %d  mid: %d \n", left, right, mid);
+        if(arr[mid] == target){
+            return mid;
+        } else if(arr[mid] < target){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 3;
+
+    int result = binarySearch(arr, n, x);
+    if(result == -1){
+        printf("Element is not present in the array");
+    } else {
+        printf("Element is present at index %d", result);
+    }
+    return 0;
+}
