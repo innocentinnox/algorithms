1818fa3cb883126f4676096d48946e24538a5f8b
innocent <innoxuganda@gmail.com>
2025-04-22 16:52:28 +0300

    selection_sort and insertion_sort

---
+++ selection_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void printArray(int arr[], int n){
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void selection_sort(int *arr, int n){
+    for (int i = 0; i < n - 1; i++){
+        int min = i;
+        for (int j = i+1; j < n; j++){
+            if(arr[j] < arr[min]) min = j;
+        }
+        if(min != i){
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+int main(){
+    int arr[] = {5, 3, 8, 6, 2, 8, 7, 4, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selection_sort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
